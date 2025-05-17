f83c8935b5a567fe16a31e1c31f0613fa0bd2be7
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-05-17 18:23:18 -0400

    finished exercise 2, had to look at solution at end but got almost there by myself

--- scripts.js
+++ scripts.js
@@ -2,11 +2,27 @@ console.log("----------Exercise 1----------");
 
 
 function camelize(toString){
-   const noDash = toString.split("-");
-   console.log(noDash);
-
-   const newString = noDash.join("");
-   console.log(newString)
+    return toString
+    //splits the string at the -, removing it.
+    .split("-")
+   //index is the iteration of the loop, ? means if..else
+   //conditional? if true : else false
+  .map((word, index)=>
+    index==0? word : word[0].toUpperCase() + word.slice(1))
+  .join("")
+  
 };
 
-camelize("background-color")
+console.log(camelize("background-color"));
+console.log(camelize("sussy-amogus"));
+
+console.log("----------Exercise 2----------");
+
+let arr = [1,2,3,4,5,6,7,8];
+
+function filterRange(arr, a, b){
+    return arr.filter((item) =>(a <= item && item <=b))
+}
+
+let filtered = filterRange(arr, 2,6)
+console.log(filtered)
