896caf251c3c21d56bdfcb8ddf6f8ced2d990418
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-05-22 22:03:00 -0400

    added comments and started exercise 3

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ console.log(camelize("background-color"));
 console.log(camelize("sussy-amogus"));
 
 console.log("----------Exercise 2----------");
-
+//filter array, get items equal or higher than a and equal or lower than b, then return them in a new array
 let arr = [1,2,3,4,5,6,7,8];
 
 function filterRange(arr, a, b){
@@ -26,3 +26,13 @@ function filterRange(arr, a, b){
 
 let filtered = filterRange(arr, 2,6)
 console.log(filtered)
+
+console.log("----------Exercise 3----------");
+//get array, removes all values except between a & b, should  modify the original array and not return anything
+let inArr = [5,3,8,1,2,4,7,9];
+
+function filterRangeInPlace(arr,a,b){
+    return arr.
+}
+
+filterRangeInPlace(inArr, 1,4)//expected to return 
